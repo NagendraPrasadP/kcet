194e30da75777445323bfb3c383e23c3a00f92bf
DESKTOP-6RCMU5O\USER <nagendra.prasad@medhand.net>
2019-10-05 22:21:07 +0530

    kcet

---
+++ cetserver.js
@@ -0,0 +1,35 @@
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+const express = require('express');
+const bodyParser = require('body-parser');
+var qs = require('querystring')
+const app = express();
+var loggedin=false;
+var port = process.env.PORT||8080;
+
+http.createServer(function (req, res) {
+	console.log(req.method);
+	app.use(bodyParser.urlencoded({ extended: false }));
+	console.log(req.connection.remoteAddress);
+	
+	
+	var q = url.parse(req.url, true);
+	var a= q.pathname.split('/');
+	
+	
+	
+	fn = "./cet.html";
+	fs.readFile(fn, function(err, data) {
+		if (err) {
+		  res.writeHead(404, {'Content-Type': 'text/html'});
+		  return res.end("404 Not Found");
+		} 
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(data);
+		return res.end();
+	  });
+	
+	
+	
+}).listen(port);
